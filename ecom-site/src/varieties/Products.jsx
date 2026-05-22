8ca93e0c0a563122e78d7294b12add07238e3409
susmita070304 <163282093+susmita070304@users.noreply.github.com>
2026-05-22 14:22:47 +0000

    products page established

--- ecom-site/src/varieties/Products.jsx
+++ ecom-site/src/varieties/Products.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import sofaImage from "./sofa.jpg";
 import curtainsImage from "./curtains.jpg";
 import backgroundImage from "./background1.jpg";
+import productsLogo from "./productsLogo.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHouse } from "@fortawesome/free-solid-svg-icons";
 
@@ -9,24 +10,29 @@ import { faHouse } from "@fortawesome/free-solid-svg-icons";
 
 export default function Products() {
   return (
-    <div className="h-[500px] w-[1520px]">
+    <div className="h-full max-w-screen">
       <div
-        className=" bg-contain  bg-no-repeat"
+        className="min-h-screen min-w-screen bg-cover bg-center bg-no-repeat flex flex-row items-center justify-center" "
         style={{ backgroundImage: `url(${backgroundImage})` }}
       >
-        <div
-          className="min-h-screen bg-auto bg-center bg-no-repeat"
-          style={{ curtainsImage: `url(${curtainsImage})` }}
-        ></div>
-        <div
-          className="min-h-screen bg-auto bg-center bg-no-repeat"
-          style={{ sofaImage: `url(${sofaImage})` }}
+
+        <div className="h-[400px] w-[400px] bg-contain bg-center bg-no-repeat flex flex-row items-center justify-start"
+          style={{ backgroundImage: `url(${productsLogo})` }}>
+
+          <div
+          className="h-[100px] w-[100px] bg-contain bg-center bg-no-repeat bg-black mr-10"
+          style={{ backgroundImage: `url(${curtainsImage})` }}
         ></div>
 
-        <h1 className="text-white text-3xl p-6">
-          Products Page
-        </h1>
-      </div>
+        <div
+          className="h-[100px] w-[100px] bg-contain bg-center bg-no-repeat bg-black mr-10"
+          style={{ backgroundImage: `url(${sofaImage})` }}
+        ></div>
+
+        </div>
+
+
+    </div>
     </div>
   );
 }
