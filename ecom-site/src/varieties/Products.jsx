40c0657bec6ca4d79c0b06a6dfef98f8fba89ee6
susmita070304 <163282093+susmita070304@users.noreply.github.com>
2026-05-22 14:52:45 +0000

    product page designing complete

--- ecom-site/src/varieties/Products.jsx
+++ ecom-site/src/varieties/Products.jsx
@@ -6,33 +6,42 @@ import productsLogo from "./productsLogo.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHouse } from "@fortawesome/free-solid-svg-icons";
 
-
-
 export default function Products() {
   return (
     <div className="h-full max-w-screen">
       <div
-        className="min-h-screen min-w-screen bg-cover bg-center bg-no-repeat flex flex-row items-center justify-center" "
+        className="min-h-screen min-w-screen bg-cover bg-center bg-no-repeat flex flex-col items-center justify-center" 
         style={{ backgroundImage: `url(${backgroundImage})` }}
       >
-
-        <div className="h-[400px] w-[400px] bg-contain bg-center bg-no-repeat flex flex-row items-center justify-start"
-          style={{ backgroundImage: `url(${productsLogo})` }}>
-
-          <div
-          className="h-[100px] w-[100px] bg-contain bg-center bg-no-repeat bg-black mr-10"
-          style={{ backgroundImage: `url(${curtainsImage})` }}
+        {/* Logo */}
+        <div 
+          className="h-[200px] w-[300px] bg-contain bg-center bg-no-repeat"
+          style={{ backgroundImage: `url(${productsLogo})` }}
         ></div>
 
-        <div
-          className="h-[100px] w-[100px] bg-contain bg-center bg-no-repeat bg-black mr-10"
-          style={{ backgroundImage: `url(${sofaImage})` }}
-        ></div>
+        {/* Products Row Container */}
+        <div className="flex flex-row items-center justify-center gap-40 mt-10">
+          
+          {/* Curtains Column (Photo + Text) */}
+          <div className="flex flex-col items-center">
+            <div
+              className="h-[285px] w-[400px] bg-contain bg-center bg-no-repeat bg-black rounded-3xl cursor-pointer"
+              style={{ backgroundImage: `url(${curtainsImage})` }}
+            ></div>
+            <h1 className="text-4xl font-bold mt-4 font-serif text-black">Curtains</h1>
+          </div>
+
+          {/* Sofas Column (Photo + Text) */}
+          <div className="flex flex-col items-center">
+            <div
+              className="h-[285px] w-[400px] bg-contain bg-center bg-no-repeat bg-black rounded-3xl cursor-pointer"
+              style={{ backgroundImage: `url(${sofaImage})` }}
+            ></div>
+            <h1 className="text-4xl font-bold mt-4 font-serif text-black">Sofas</h1>
+          </div>
 
         </div>
-
-
-    </div>
+      </div>
     </div>
   );
 }
